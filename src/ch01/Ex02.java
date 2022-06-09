798ba4c1c6db49380a7c8990ae2eb9718824f2a7
신지연 <skyblue6393@naver.com>
2022-06-09 11:24:05 +0900

    Ex02 프로그램 생성   -> 지금까지 배운것들을 sysout을 통해 나열함

---
+++ src/ch01/Ex02.java
@@ -0,0 +1,22 @@
+package ch01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1. 깃허브에 가입");
+		System.out.println("2. 소스트리를 설치");
+		System.out.println("3. 소스트리를 둘러보기");
+		System.out.println("4. 깃허브를 둘러보기");
+		System.out.println("5. 혼자서 깃 해보기");
+		System.out.println("5-1. local_2022-06-09를 레파지토리화");
+		System.out.println("5-2. local_2022-06-09에 여러 작업");
+		System.out.println("5-3. 커밋, 커밋 메세지에 대해서 배움");
+		System.out.println("5-4. 버전을 옮겨다니면서 과거의 작업으로 돌아갈 수 있다는걸 배움");
+		System.out.println("5-5. 이클립스에 자바 프로젝트 생성 & 레파지토리화");
+		System.out.println("5-6. .gitignore에 대해서 배움");
+		
+	}
+
+}
