f40eb4a1c09b050b96814076eec4fb7593f76b01
DESKTOP-7KDHQKS\USER <0813bbb@naver.com>
2022-06-09 11:39:56 +0900

    Ex02.java 수정   -> 중간에 누락된 사항(5번) 추가

--- src/ch01/Ex02.java
+++ src/ch01/Ex02.java
@@ -9,13 +9,14 @@ public class Ex02 {
 		System.out.println("2. 소스트리를 설치");
 		System.out.println("3. 소스트리를 둘러보기");
 		System.out.println("4. 깃허브를 둘러보기");
-		System.out.println("5. 혼자서 깃 해보기");
-		System.out.println("5-1. local_2022-06-09를 레파지토리화");
-		System.out.println("5-2. local_2022-06-09에 여러 작업");
-		System.out.println("5-3. 커밋, 커밋 메세지에 대해서 배움");
-		System.out.println("5-4. 버전을 옮겨다니면서 과거의 작업으로 돌아갈 수 있다는걸 배움");
-		System.out.println("5-5. 이클립스에 자바 프로젝트 생성 & 레파지토리화");
-		System.out.println("5-6. .gitignore에 대해서 배움");
+		System.out.println("5. 깃허브 레파지토리 생성");
+		System.out.println("6. 혼자서 깃 해보기");
+		System.out.println("6-1. local_2022-06-09를 레파지토리화");
+		System.out.println("6-2. local_2022-06-09에 여러 작업");
+		System.out.println("6-3. 커밋, 커밋 메세지에 대해서 배움");
+		System.out.println("6-4. 버전을 옮겨다니면서 과거의 작업으로 돌아갈 수 있다는걸 배움");
+		System.out.println("6-5. 이클립스에 자바 프로젝트 생성 & 레파지토리화");
+		System.out.println("6-6. .gitignore에 대해서 배움");
 		
 	}
 
